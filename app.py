8f5868ea10293e3ad589fa6dc477fae409afb68e
GoodyeAAr <gauthier.cherchye@partenamut.be>
2021-05-06 09:43:12 +0000

    feat(app): render template

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 """Simple example"""
 from __future__ import absolute_import
 
-from flask import Flask
-from flask.helpers import make_response
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    response = make_response('<h1>This document carries a cookie!</h1>')
-    response.set_cookie('answer', '42')
+    return render_template('index.html')
 
-    return response
-
-@app.route('/user/<id>')
-def get_user(name):
-    return f'<h1>Hello {name} ! </h1>'
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
