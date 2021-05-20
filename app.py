a50edd31f94ad1fe61a3cac4c539885b57c0fb21
GoodyeAAr <gauthier.cherchye@partenamut.be>
2021-05-20 10:56:00 +0000

    feat(html): add moment

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 """Simple example"""
 from __future__ import absolute_import
 
+from datetime import datetime
+
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 from flask import Flask, render_template
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
